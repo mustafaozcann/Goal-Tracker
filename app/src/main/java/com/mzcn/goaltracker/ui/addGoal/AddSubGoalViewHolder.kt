3516eb49703e3e318f3ac7bc1920df666b6ea684
Mustafa Özcan <mstffaozcnn@gmail.com>
2023-04-30 18:35:56 +0300

    Update add goal screen

--- app/src/main/java/com/mzcn/goaltracker/ui/addGoal/AddSubGoalViewHolder.kt
+++ app/src/main/java/com/mzcn/goaltracker/ui/addGoal/AddSubGoalViewHolder.kt
@@ -1,7 +1,12 @@
 package com.mzcn.goaltracker.ui.addGoal
 
-import android.app.Application
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.CalendarView
+import android.widget.EditText
 import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelStoreOwner
@@ -10,6 +15,9 @@ import com.mzcn.goaltracker.databinding.AddSubgoalRecyclerItemBinding
 import com.mzcn.goaltracker.databinding.FragmentAddGoalBinding
 import com.mzcn.goaltracker.databinding.LayoutAddSubgoalContainerBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Calendar
+import java.util.Date
+import javax.xml.datatype.DatatypeConstants.MONTHS
 
 @AndroidEntryPoint
 class AddSubGoalViewHolder(
@@ -23,11 +31,38 @@ class AddSubGoalViewHolder(
     fun bind() {
         viewModel =
             ViewModelProvider(containerBinding.root.context as ViewModelStoreOwner)[AddGoalViewModel::class.java]
+
+
         addView(containerBinding)
+    }
+
+    private fun setDate(edittext: EditText) {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+
+        DatePickerDialog(binding.root.context,
+            { view, year, monthOfYear, dayOfMonth ->
+
+                edittext.setText(String.format("%02d/%02d/%d", dayOfMonth, monthOfYear + 1, year))
+
+            }, year, month, day
+        ).show()
     }
 
     private fun addView(containerBinding: LayoutAddSubgoalContainerBinding) {
         with(containerBinding) {
+
+            subGoalStartDateEdittext.setOnClickListener {
+                setDate(it as EditText)
+            }
+
+            subGoalEndDateEdittext.setOnClickListener {
+                setDate(it as EditText)
+            }
+
             addSubgoalImageButton.setOnClickListener {
                 subgoalEdittextLayout.isVisible = true
                 startDateEdittextLayout.isVisible = true
@@ -37,7 +72,7 @@ class AddSubGoalViewHolder(
 
                 val goalText = addGoalBinding.goalEdittext.text.toString()
 
-                if(viewModel.isValidGoal(goalText)){
+                if (viewModel.isValidGoal(goalText)) {
                     addSubgoalButton.setOnClickListener {
                         val view =
                             LayoutAddSubgoalContainerBinding.inflate(LayoutInflater.from(binding.root.context))
@@ -49,13 +84,15 @@ class AddSubGoalViewHolder(
 
                         addView(view)
                     }
+                } else {
+                    addGoalBinding.goalEdittextLayout.error = "Alan boş bırakılamaz !"
                 }
-
-
             }
         }
 
+
         binding.subGoalLayout.addView(containerBinding.root)
+
     }
 
 }
